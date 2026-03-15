2e5a90806ed252238c9fae47dea7fb45f3110622
KevillaAguiar <kevilaaguiar@gmail.com>
2026-03-14 21:31:40 -0300

    Refactor scaled_dot_product_attention function to include optional mask parameter and improve matrix operations

--- Lab1/laboratorio_1.py
+++ Lab1/laboratorio_1.py
@@ -1,15 +1,18 @@
 import numpy as np
 
-def scaled_dot_product_attention(Q, K, V):
+def scaled_dot_product_attention(Q, K, V, mask=None):
 
-    product_scalar = np.matmul(Q, K.T)
+    product_scalar = np.matmul(Q, K.transpose(0, 2, 1))
 
-    d_k = K.shape[1]
+    d_k = K.shape[-1]
 
     scaled_scores = product_scalar/np.sqrt(d_k)
 
+    if mask is not None:
+        scaled_scores = scaled_scores + mask
+
     exp_scores = np.exp(scaled_scores)
-    sums = np.sum(exp_scores, axis=1, keepdims=True)
+    sums = np.sum(exp_scores, axis=-1, keepdims=True)
     attention_weights = exp_scores/sums
 
     output = attention_weights @ V
